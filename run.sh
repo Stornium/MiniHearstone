cedcf75e8e018a64ae0f4fe03e59d39480585e1c
paul <paul604@users.noreply.github.com>
2018-01-12 20:55:52 +0100

    script ok

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ dir="target/class";
 if [ ! -f $dir ]; then
         mkdir -p $dir
 fi
-javac -sourcepath src/main/java/ -classpath $dir src/main/java/fr/nicoPaul/miniHearstone/*.java -d $dir
+javac -sourcepath src/main/java/ -classpath $dir src/main/java/fr/nicoPaul/*/*.java -d $dir
 
-#java -classpath $dir fr.nicoPaul.miniHearstone.app
+java -classpath $dir fr.nicoPaul.miniHearstone.app
